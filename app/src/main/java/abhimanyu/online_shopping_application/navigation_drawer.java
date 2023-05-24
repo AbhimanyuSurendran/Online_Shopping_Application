7886d0099b217b655640049f75749bb149e7a4e0
AbhimanyuSurendran <abhiyoyo165@gmail.com>
2023-05-24 19:56:13 +0530

    commit

--- app/src/main/java/abhimanyu/online_shopping_application/navigation_drawer.java
+++ app/src/main/java/abhimanyu/online_shopping_application/navigation_drawer.java
@@ -1,8 +1,13 @@
 package abhimanyu.online_shopping_application;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class navigation_drawer extends AppCompatActivity {
 
@@ -10,5 +15,6 @@ public class navigation_drawer extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigation_drawer);
+
     }
 }
