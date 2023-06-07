a3b7fe8ea7a55131d996ecd4f1e1b2db19138680
AbhimanyuSurendran <abhiyoyo165@gmail.com>
2023-06-07 14:44:02 +0530

    update

---
+++ app/src/main/java/abhimanyu/online_shopping_application/content.java
@@ -0,0 +1,12 @@
+package abhimanyu.online_shopping_application;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class content extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content);
+}
